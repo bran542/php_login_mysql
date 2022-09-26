df6d779fb5ba048c0e7da9d1d31737b6a35e3ca7
Brandon M <brandonmunoz542@gmail.com>
2022-09-26 03:53:55 -0300

    Enlaces de registro y estilos css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to your Application</title>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <h1>Please Login or SignUp</h1>
+        <a href="login.php">Login</a> or
+        <a href="signup.php">SignUp</a>
+    </body>
+</html>
